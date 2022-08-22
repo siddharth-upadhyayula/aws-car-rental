4c2395801faf5c4270e193e3645fcb9b304e125f
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-08-21 21:05:28 -0400

    Create Amplify project

---
+++ src/ui-components/AddCar.jsx
@@ -0,0 +1,200 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import {
+  Button,
+  Divider,
+  Flex,
+  Text,
+  TextField,
+  View,
+} from "@aws-amplify/ui-react";
+export default function AddCar(props) {
+  const { overrides, ...rest } = props;
+  return (
+    <Flex
+      gap="16px"
+      direction="column"
+      width="640px"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...rest}
+      {...getOverrideProps(overrides, "AddCar")}
+    >
+      <Flex
+        gap="24px"
+        direction="column"
+        shrink="0"
+        alignSelf="stretch"
+        objectFit="cover"
+        position="relative"
+        padding="24px 24px 24px 24px"
+        {...getOverrideProps(overrides, "Content")}
+      >
+        <Flex
+          gap="16px"
+          alignItems="center"
+          shrink="0"
+          alignSelf="stretch"
+          objectFit="cover"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Edit Profile")}
+        >
+          <View
+            width="24px"
+            height="24px"
+            shrink="0"
+            overflow="hidden"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            {...getOverrideProps(overrides, "Icon")}
+          ></View>
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="700"
+            color="rgba(13,26,38,1)"
+            lineHeight="20px"
+            textAlign="left"
+            display="flex"
+            direction="column"
+            justifyContent="flex-start"
+            shrink="0"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="Add Car"
+            {...getOverrideProps(overrides, "Add Car")}
+          ></Text>
+        </Flex>
+        <Divider
+          height="1px"
+          shrink="0"
+          alignSelf="stretch"
+          objectFit="cover"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          size="small"
+          orientation="horizontal"
+          {...getOverrideProps(overrides, "Divider29766917")}
+        ></Divider>
+        <Flex
+          gap="16px"
+          direction="column"
+          shrink="0"
+          alignSelf="stretch"
+          objectFit="cover"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Forms")}
+        >
+          <TextField
+            display="flex"
+            gap="8px"
+            direction="column"
+            justifyContent="center"
+            shrink="0"
+            alignSelf="stretch"
+            objectFit="cover"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            label="Name"
+            placeholder="Toyota - Prius"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            {...getOverrideProps(overrides, "TextField29766922")}
+          ></TextField>
+          <TextField
+            display="flex"
+            gap="8px"
+            direction="column"
+            justifyContent="center"
+            shrink="0"
+            alignSelf="stretch"
+            objectFit="cover"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            label="Price per day (USD)"
+            placeholder="45"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            {...getOverrideProps(overrides, "TextField29766923")}
+          ></TextField>
+          <TextField
+            display="flex"
+            gap="8px"
+            direction="column"
+            justifyContent="center"
+            shrink="0"
+            alignSelf="stretch"
+            objectFit="cover"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            label="Description"
+            placeholder="4 seats"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            {...getOverrideProps(overrides, "TextField29766924")}
+          ></TextField>
+          <TextField
+            display="flex"
+            gap="8px"
+            direction="column"
+            justifyContent="center"
+            shrink="0"
+            alignSelf="stretch"
+            objectFit="cover"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            label="Image Link"
+            placeholder="Image url"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            {...getOverrideProps(overrides, "TextField31462694")}
+          ></TextField>
+        </Flex>
+        <Divider
+          height="1px"
+          shrink="0"
+          alignSelf="stretch"
+          objectFit="cover"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          size="small"
+          orientation="horizontal"
+          {...getOverrideProps(overrides, "Divider29766925")}
+        ></Divider>
+        <Button
+          display="flex"
+          gap="0"
+          width="fit-content"
+          justifyContent="center"
+          alignItems="center"
+          shrink="0"
+          position="relative"
+          size="default"
+          isDisabled={false}
+          variation="primary"
+          children="Save"
+          {...getOverrideProps(overrides, "Button")}
+        ></Button>
+      </Flex>
+    </Flex>
+  );
+}
