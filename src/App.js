d7087d58d38e21d01f86fde8b83ee2700b8f10a7
sid_upadhyayula <s.upadhyayula1@ufl.edu>
2022-08-21 21:45:18 -0400

    Add the card components to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import {NavBar} from './ui-components';
+import {NavBar, NewCarsCollection} from './ui-components';
 import './App.css';
 
 function App() {
+
+  const navbarOverrides = {
+    "Flex.Flex[0].Image[0]": {
+      src: "https://img.icons8.com/color/50/000000/car--v1.png",
+    },
+    
+    
+  };
+
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar overrides={navbarOverrides} width='100%' />
       <header className="App-header">
-        <h1>Car Rental App</h1>
+        <NewCarsCollection />
       </header>
     </div>
   );
